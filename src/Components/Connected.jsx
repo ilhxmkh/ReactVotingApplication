cda1bbf0763ef1c5ad3b04ad174fe6818161c5c8
adnanansari456 <100843263+adnanansari456@users.noreply.github.com>
2023-12-03 18:05:38 +0530

    Css added 2

--- src/Components/Connected.jsx
+++ src/Components/Connected.jsx
@@ -1,58 +1,100 @@
-import React, { useState } from "react";
+import React from "react";
 import Login from "./Login";
 import Header from "./Header";
+
 const Connected = (props) => {
-    
-    
-
-    
-    
-    
     return (
-        <><Header/>
-        <div className="connected-container">
-            <Login  connectWallet = {props.connectWallet}
-                     isConnected= {props.isConnected}/>
-            {props.isConnected? <>
-            <h1 className="connected-header">You are Connected to Metamask</h1>
-            <p className="connected-account">Metamask Account: {props.account}</p>
-            <p className="connected-account">Remaining Time: {props.remainingTime}</p>
-            { props.showButton ? (
-                <p className="connected-account">You have already voted</p>
-            ) : (
-                <div>
-                    <input type="number" placeholder="Entern Candidate Index" value={props.number} onChange={props.handleNumberChange}></input>
-            <br />
-            <button className="login-button" onClick={props.voteFunction}>Vote</button>
-            
+        <>
+            {props.isConnected && <Header />}
+            <div className="connected-container">
+                <div className="connected-info">
+                    <Login
+                        connectWallet={props.connectWallet}
+                        isConnected={props.isConnected}
+                    />
+                    {props.isConnected ? (
+                        <>
+                            <div className="connected-header">
+                                You are Connected to Metamask
+                            </div>
+                            <svg xmlns="http://www.w3.org/2000/svg" version="1.1">
+                                <defs>
+                                    <filter id="squiggly-0">
+                                        <feTurbulence id="turbulence" baseFrequency="0.02" numOctaves="3" result="noise" seed="0" />
+                                        <feDisplacementMap id="displacement" in="SourceGraphic" in2="noise" scale="6" />
+                                    </filter>
+                                    <filter id="squiggly-1">
+                                        <feTurbulence id="turbulence" baseFrequency="0.02" numOctaves="3" result="noise" seed="1" />
+                                        <feDisplacementMap in="SourceGraphic" in2="noise" scale="8" />
+                                    </filter>
 
+                                    <filter id="squiggly-2">
+                                        <feTurbulence id="turbulence" baseFrequency="0.02" numOctaves="3" result="noise" seed="2" />
+                                        <feDisplacementMap in="SourceGraphic" in2="noise" scale="6" />
+                                    </filter>
+                                    <filter id="squiggly-3">
+                                        <feTurbulence id="turbulence" baseFrequency="0.02" numOctaves="3" result="noise" seed="3" />
+                                        <feDisplacementMap in="SourceGraphic" in2="noise" scale="8" />
+                                    </filter>
+
+                                    <filter id="squiggly-4">
+                                        <feTurbulence id="turbulence" baseFrequency="0.02" numOctaves="3" result="noise" seed="4" />
+                                        <feDisplacementMap in="SourceGraphic" in2="noise" scale="6" />
+                                    </filter>
+                                </defs>
+                            </svg>
+                            <p className="connected-account">
+                                Metamask Account: {props.account}
+                            </p>
+                            <p className="connected-account">
+                                Remaining Time: {props.remainingTime}
+                            </p>
+                            {props.showButton ? (
+                                <p className="connected-account">You have already voted</p>
+                            ) : (
+                                <div>
+                                    <input
+                                        type="number"
+                                        placeholder="Enter Candidate Index"
+                                        value={props.number}
+                                        onChange={props.handleNumberChange}
+                                    />
+                                    <br />
+                                    <button
+                                        className="login-button"
+                                        onClick={props.voteFunction}
+                                    >
+                                        Vote
+                                    </button>
+                                </div>
+                            )}
+
+                            <div className="candidates-table-container">
+                                <table id="myTable" className="candidates-table">
+                                    <thead>
+                                        <tr>
+                                            <th>Index</th>
+                                            <th>Candidate name</th>
+                                            <th>Candidate votes</th>
+                                        </tr>
+                                    </thead>
+                                    <tbody>
+                                        {props.candidates?.map((candidate, index) => (
+                                            <tr key={index}>
+                                                <td>{candidate.index}</td>
+                                                <td>{candidate.name}</td>
+                                                <td>{candidate.voteCount}</td>
+                                            </tr>
+                                        ))}
+                                    </tbody>
+                                </table>
+                            </div>
+                        </>
+                    ) : null}
                 </div>
-            )}
-            
-            <table id="myTable" className="candidates-table">
-                <thead>
-                <tr>
-                    <th>Index</th>
-                    <th>Candidate name</th>
-                    <th>Candidate votes</th>
-                </tr>
-                </thead>
-                <tbody>
-                {props.candidates?.map((candidate, index) => (
-                    <tr key={index}>
-                    <td>{candidate.index}</td>
-                    <td>{candidate.name}</td>
-                    <td>{candidate.voteCount}</td>
-                    </tr>
-                ))}
-                </tbody>
-            </table>
-            
-        
-    
-    </> : null}
-    </div>
-    </>)
-}
+            </div>
+        </>
+    );
+};
 
 export default Connected;
--- src/Components/Header.jsx
+++ src/Components/Header.jsx
@@ -1,103 +1,89 @@
 import React from 'react';
 import Container from './Container';
-// import { useSelector } from 'react-redux';
 import { useNavigate } from 'react-router-dom';
 import { Link } from 'react-router-dom';
 
-
-
 function Header() {
+  const navigate = useNavigate();
 
-const navigate = useNavigate() 
+  const styles = {
+    nav: {
+      display: 'flex',
+      flexDirection: 'row',
+      alignItems: 'center',
+    },
+    listItem: {
+      marginRight: '8px',
+    },
+    link: {
+      textDecoration: 'none',
+      color: 'white',
+    },
+    button: {
+      display: 'inline-block',
+      cursor: 'pointer',
+      padding: '8px 16px',
+      borderRadius: '4px',
+      border: 'none',
+      marginLeft: '0px',
+      transition: 'background-color 0.2s',
+      
+    },
+    activeButton: {
+      backgroundColor: '#80006a',
+      color: 'white',
+    },
+    ul: {
+      position: 'fixed',
+      top: 0,
+      listStyleType: 'none',
+      margin: 0,
+      padding: 0,
+      display: 'flex',
+      backgroundColor: 'black',
+      width: '100%',
+      left: 0,
+    },
+  };
 
+  const navItems = [
+    { name: 'Home', slug: '/', active: true },
+    { name: 'Vote', slug: '/vote', active: true },
+    { name: 'AdminPage', slug: '/adminPage', active: true },
+    { name: 'Result', slug: '/result', active: true },
+  ];
 
-const styles = {
-  nav: {
-    display: 'flex',
-  },
-  listItem: {
-    marginRight: '8px',
-  },
-  link: {
-    textDecoration: 'none',
-    color: 'white',
-  },
-  button: {
-    display: 'inline-block',
-    padding: '8px 16px',
-    borderRadius: '20px',
-    transition: 'background-color 0.2s',
-  },
-  ul: {
-    listStyleType: 'none',
-    margin: 0,
-    padding: 0,
-    display : 'flex',
-  },
-}
-const navItems = [
-  {
-      name: 'Home',
-      slug: "/",
-      active: true
-    }, 
-    {
-      name: "Vote",
-      slug: "/vote",
-      active: true,
-  },
-    
-  {
-      name: "AdminPage",
-      slug: "/adminPage",
-      active: true,
-  },
- 
-{
-  name: "Result",
-  slug: "/result",
-  active: true,
-},
-
-  ]
-
+  // Filter the navItems to keep only the first four
+  const filteredNavItems = navItems.slice(0, 4);
 
   return (
-    <header className='py-3 shadow bg-gray-500'>
+    <header className="py-3 shadow bg-gray-500">
       <Container>
         <nav style={styles.nav}>
           <div style={styles.listItem}>
-            <Link to="/" style={styles.link}>
-              Home
-              </Link>
+            {/* <div className="bg-image"></div> */}
+            <Link to="/" style={styles.link}></Link>
           </div>
-          <ul className='flex ml-auto'style={styles.nav, styles.ul}>
-            {navItems.map((item) => 
-            item.active && (
+          <ul className="flex ml-auto" style={styles.nav, styles.ul}>
+            {filteredNavItems.map((item) => (
               <li key={item.name} style={styles.listItem}>
                 <button
-                onClick={() => navigate(item.slug)}
-                style={{
-                  ...styles.button,
-                  backgroundColor: item.active ? 'blue' : 'transparent',
-                }}
-                className='inline-bock px-6 py-2 duration-200 hover:bg-blue-100 rounded-full'
-                >{item.name}
+                  onClick={() => navigate(item.slug)}
+                  style={{
+                    ...styles.button,
+                    ...(item.active ? styles.activeButton : {}),
+                  }}
+                  className="inline-bock px-6 py-2 duration-200 hover:bg-blue-100 rounded-full"
+                >
+                  {item.name}
                 </button>
               </li>
-            )
-            )}
-            {/* {authStatus && (
-              <li>
-                <LogoutBtn />
-              </li>
-            )} */}
+            ))}
           </ul>
         </nav>
-        </Container>
+      </Container>
     </header>
-  )
-
+  );
 }
 
 export default Header;
